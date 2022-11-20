89390978b42b1cb49084d63d2ba6ae280f3ce0b0
HamidRazaVision <hamidraza.chemistry@gmail.com>
2022-11-20 06:44:12 -0800

    responsive desgin media query done

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,45 @@
-import React from 'react';
+import React from "react";
 import styles from "../styles/header.module.css";
-
+import image from "./../images/Photo.png";
+import insta from "./../images/insta-logo.png";
+import behance from "./../images/behance-logo.png";
+import dribble from "./../images/dribble-logo.png";
 function Header() {
-    return ( 
+  return (
+    <div className={styles.container}>
+      <div className={styles.hero_container}>
+        <div className={styles.text_container}>
 
-        <div className = {styles.container}>
-
-            header container
-
+            <h1>Hi, I’m Raza</h1>
+            <p>
+              I design beautiful websites & mobile apps that modern trends
+              demand
+            </p>
+            <a href="#"> Contact Me</a>
+        
         </div>
-     );
+        <div className={styles.image_container}>
+          <div className={styles.main_image}>
+            <img src={image.src} alt="hero-image" />
+            <div></div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.social_container}>
+        <div className={styles.social_icons}>
+          <a href="#">
+            <img src={insta.src} alt="instagram logo" />
+          </a>
+          <a href="#">
+            <img src={behance.src} alt="" />
+          </a>
+          <a href="#">
+            <img src={dribble.src} alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
